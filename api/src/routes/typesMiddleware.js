dac21ca3fcee96bb54b42449ee7b60eeb84d755e
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-11-07 17:47:29 -0300

    adding route types

--- api/src/routes/typesMiddleware.js
+++ api/src/routes/typesMiddleware.js
@@ -1,15 +1,28 @@
 const router = require("express").Router();
 const { Type } = require("../db");
 const { getAllPokemons } = require("./Controllers/controllers");
+const axios = require("axios");
 
 router.get("/", async (req, res) => {
   try {
-    const cantidadTypes = await Type.count(); // cuantos hay en la db
-    const allPokemons = await getAllPokemons();
-    const pokeMapeados = allPokemons?.map((el) => el.types);
-    res.status(200).send(pokeMapeados);
-  } catch (err) {
-    res.send({ msg: err.message });
+    const apiPromise = await axios.get("https://pokeapi.co/api/v2/type"); // traigo todos los tipos de pokemones
+    let typesFromApi = apiPromise.data.results.map((t) => t.name).sort(); //tipos por orden alfabetico
+
+    const pokeTypes = await Type.findAll({ attributes: ["id", "name"] });
+
+    if (pokeTypes.length > 0) {
+      return res.status(200).json(pokeTypes); // si tengo algo ya en la db entonces lo retorno, es decir los tipos ya
+    } else {
+      typesFromApi.forEach((t) => {
+        // si no los tengo entonces los creo con el metodo bulkcreate querecibe un array y va creando a todos los que reecibi por medio de la api (typesFromApi)
+        Type.bulkCreate([{ name: t }]);
+      });
+      // ya para este paso estan ccreados entonces los busco y simplemento los retorno junto cccon su id
+      const typesAdded = await Type.findAll({ attributes: ["id", "name"] });
+      return res.status(200).json(typesAdded);
+    }
+  } catch (error) {
+    res.status(404).send("No se encontraron Tipos de Pokemones");
   }
 });
 
