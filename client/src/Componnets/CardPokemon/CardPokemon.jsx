67c791d06e306ccd2bea58b98cf56ef1f1a30ca3
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-11-09 00:13:44 -0300

    addind to style card for type

--- client/src/Componnets/CardPokemon/CardPokemon.jsx
+++ client/src/Componnets/CardPokemon/CardPokemon.jsx
@@ -4,7 +4,7 @@ import "./CardPokemon.css";
 
 const CardPokemon = ({ name, image, types, pokeId }) => {
   return (
-    <article className="contCard">
+    /*   <article className="contCard">
       <Link className="contCard" to={`/pokemons/${pokeId}`}>
         <h2 className="name">{name}</h2>
         <picture className="card-img-container">
@@ -22,7 +22,30 @@ const CardPokemon = ({ name, image, types, pokeId }) => {
           ))}
         </div>
       </Link>
-    </article>
+    </article> */
+    <>
+      {types?.map((t) => (
+        <article key={pokeId + t} className={`${t} containerCard`}>
+          <Link className="contCard" to={`/pokemons/${pokeId}`}>
+            <h2 className="name">{name}</h2>
+            <picture className="card-img-container">
+              <img
+                src={image ? image : "NOCUENTA CCON IMAGEN"}
+                alt="NOCUENTA CCON IMAGEN"
+                className="img"
+              />
+            </picture>
+            <div className="types">
+              {types?.map((t) => (
+                <p key={pokeId + t} className={t}>
+                  {t}
+                </p>
+              ))}
+            </div>
+          </Link>
+        </article>
+      ))}
+    </>
   );
 };
 
