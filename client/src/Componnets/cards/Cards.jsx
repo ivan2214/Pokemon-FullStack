3f9c27bab9e4b8046f654bdcb1be799d2abe2460
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-11-13 21:23:16 -0300

    addind to commit

--- client/src/Componnets/cards/Cards.jsx
+++ client/src/Componnets/cards/Cards.jsx
@@ -14,13 +14,13 @@ const Cards = () => {
   const [pokesPorPagina] = useState(12);
   const ultimoPokemon = paginaActual * pokesPorPagina; // 1 * 12 > 12  --------- 2*12 > 24 asi sucesivamente
   const primerPokemon = ultimoPokemon - pokesPorPagina; // 12 - 12 > 12 --------- 24 - 12 > 12 asi sucesivamente
-  const pokemonsPaginados =
-    pokemons.length > 1
-      ? pokemons.slice(primerPokemon, ultimoPokemon)
-      : pokemons;
+  const pokemonsPaginados = pokemons.length
+    ? pokemons.slice(primerPokemon, ultimoPokemon)
+    : pokemons;
   const totalPokemons = pokemons.length;
   const dispatch = useDispatch();
-
+  console.log(pokemonsPaginados);
+  console.log(pokemons);
   useEffect(() => {
     dispatch(getTypes());
   }, [dispatch]);
@@ -32,7 +32,7 @@ const Cards = () => {
   function reset() {
     dispatch(getAllPokemons());
   }
-  console.log(pokemons);
+
   return (
     <>
       <Paginacion
@@ -44,7 +44,7 @@ const Cards = () => {
       />
       {loading ? (
         <Spinner />
-      ) : pokemons.length ? (
+      ) : pokemonsPaginados.length ? (
         <section className="cards-container">
           {pokemonsPaginados?.map((p) => (
             <CardPokemon
